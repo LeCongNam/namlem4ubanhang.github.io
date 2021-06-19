10ecf32871e63ba7e802aa68f8f823d4fdc25d2b
namlem4u <namlem4u@gmail.com>
2021-06-19 16:15:33 +0700

    firt commit

---
+++ wp-login.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="vi">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Đăng nhập &lsaquo; DIENTHOAIRE &#8212; WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="dns-prefetch" href="//s.w.org">
+<link rel="dns-prefetch" href="//c0.wp.com">
+<script src="https://c0.wp.com/c/5.7.2/wp-includes/js/jquery/jquery.min.js" id="jquery-core-js"></script>
+<script src="https://c0.wp.com/c/5.7.2/wp-includes/js/jquery/jquery-migrate.min.js" id="jquery-migrate-js"></script>
+<link rel="stylesheet" id="dashicons-css" href="https://c0.wp.com/c/5.7.2/wp-includes/css/dashicons.min.css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://c0.wp.com/c/5.7.2/wp-includes/css/buttons.min.css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://c0.wp.com/c/5.7.2/wp-admin/css/forms.min.css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://c0.wp.com/c/5.7.2/wp-admin/css/l10n.min.css" media="all">
+<link rel="stylesheet" id="login-css" href="https://c0.wp.com/c/5.7.2/wp-admin/css/login.min.css" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-vi">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://vi.wordpress.org/">Xây dựng bằng WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Tên người dùng hoặc Địa chỉ Email</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Mật khẩu</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Hiện mật khẩu">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Tự động đăng nhập</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Đăng nhập">
+									<input type="hidden" name="redirect_to" value="/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="/tai-khoan/lost-password/">Bạn quên mật khẩu?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="/">&larr; Quay lại DIENTHOAIRE</a>		</p>
+			</div>
+	<script id="ce4wp_form_submit-js-extra">
+var ce4wp_form_submit_data = {"siteUrl":"","url":"\/wp-admin\/admin-ajax.php","nonce":"ef31833421","listNonce":"9f161901de"};
+</script>
+<script src="/wp-content/plugins/creative-mail-by-constant-contact/assets/js/block/submit.js?ver=1624061574" id="ce4wp_form_submit-js"></script>
+<script id="zxcvbn-async-js-extra">
+var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src="https://c0.wp.com/c/5.7.2/wp-includes/js/zxcvbn-async.min.js" id="zxcvbn-async-js"></script>
+<script src="https://c0.wp.com/c/5.7.2/wp-includes/js/dist/vendor/wp-polyfill.min.js" id="wp-polyfill-js"></script>
+<script id="wp-polyfill-js-after">
+( 'fetch' in window ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );( 'objectFit' in document.documentElement.style ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-object-fit.min.js?ver=2.3.4"><\/scr' + 'ipt>' );
+</script>
+<script src="https://c0.wp.com/c/5.7.2/wp-includes/js/dist/hooks.min.js" id="wp-hooks-js"></script>
+<script src="https://c0.wp.com/c/5.7.2/wp-includes/js/dist/i18n.min.js" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Mật khẩu mạnh không xác định","short":"Rất yếu","bad":"Yếu","good":"Trung bình","strong":"Mạnh","mismatch":"Mật khẩu không khớp"};
+</script>
+<script id="password-strength-meter-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", { "locale_data": { "messages": { "": {} } } } );
+</script>
+<script src="https://c0.wp.com/c/5.7.2/wp-admin/js/password-strength-meter.min.js" id="password-strength-meter-js"></script>
+<script src="https://c0.wp.com/c/5.7.2/wp-includes/js/underscore.min.js" id="underscore-js"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src="https://c0.wp.com/c/5.7.2/wp-includes/js/wp-util.min.js" id="wp-util-js"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"621ddcafb5"};
+</script>
+<script id="user-profile-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", { "locale_data": { "messages": { "": {} } } } );
+</script>
+<script src="https://c0.wp.com/c/5.7.2/wp-admin/js/user-profile.min.js" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
